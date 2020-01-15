5fdd36173c7a4e26cf3035df211b23f38612b5d9
saurabh <ssroyal>
2020-01-15 18:25:03 +0530

    Edit CreateNewAccount method Add Create PIN method

--- src/IOBbank/YourAccount.java
+++ src/IOBbank/YourAccount.java
@@ -1,11 +1,28 @@
 package IOBbank;
 
+import java.util.Scanner;
+
 public class YourAccount {
     long Accountno = 00000;
 
     public void CreateNewAccount(String Name) {
         Accountno = Accountno + 1;
-        System.out.println("Hey Saurabh Account Sucessfully Created Account no is" + Accountno);
+        System.out.println("Hey " + Name + " Your Account Sucessfully Created Account no is" + Accountno);
+
+    }
+
+    public void CreatePin(int PIN) {
+        System.out.println("Re Enter Your PIN");
+        Scanner sc = new Scanner(System.in);
+        int PIN2 = sc.nextInt();
+        if (PIN == PIN2) {
+            System.out.println("PIN created");
+
+        } else {
+            System.out.println("Mismathched");
+            CreatePin(PIN);
+
+        }
 
 
     }
