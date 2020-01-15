988f0fbcb4938accafdcca6c12add0535f10235d
saurabh <ssroyal>
2020-01-15 16:34:05 +0530

    Initialize abstract methods of ATM innterface

--- src/Adt/ATM.java
+++ src/Adt/ATM.java
@@ -1,5 +1,12 @@
 package Adt;
 
 public interface ATM {
+    public abstract double deposit(double amount);
+
+    public abstract double withdraw(double amount);
+
+    public abstract void showBalance();
+
+    public abstract boolean verifyPin(int atmPin);
 
 }
