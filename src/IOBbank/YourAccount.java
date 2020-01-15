fcc4fc36747a119c9160f52c33a409087e1d2324
saurabh <ssroyal>
2020-01-15 18:45:48 +0530

    Add logic to store Account and PIN in A 2D array

--- src/IOBbank/YourAccount.java
+++ src/IOBbank/YourAccount.java
@@ -3,11 +3,17 @@ package IOBbank;
 import java.util.Scanner;
 
 public class YourAccount {
-    long Accountno = 00000;
+    long totalaccounts = 00000;
+    int i = 0;
+    long AccountArray[][];
 
     public void CreateNewAccount(String Name) {
-        Accountno = Accountno + 1;
+        totalaccounts = totalaccounts + 1;
+        long Accountno = totalaccounts;
+        AccountArray[i][0] = Accountno;
+        i = i + 1;
         System.out.println("Hey " + Name + " Your Account Sucessfully Created Account no is" + Accountno);
+
 
     }
 
@@ -17,6 +23,7 @@ public class YourAccount {
         int PIN2 = sc.nextInt();
         if (PIN == PIN2) {
             System.out.println("PIN created");
+            AccountArray[i][1] = PIN;
 
         } else {
             System.out.println("Mismathched");
