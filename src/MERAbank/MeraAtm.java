11d0ca53268402946264a726a857a337c58ed887
saurabh <ssroyal>
2020-01-15 22:22:26 +0530

    Final Commit

---
+++ src/MERAbank/MeraAtm.java
@@ -0,0 +1,53 @@
+package MERAbank;
+
+import Adt.ATM;
+
+public class MeraAtm implements ATM {
+    private int Pin = 1234;
+    private double AccountBalance = 0000;
+
+    @Override
+    public double deposit(double amount) {
+        if (amount > 0) {
+            this.AccountBalance = amount + AccountBalance;
+            System.out.println("Rs." + amount + " Deposited");
+            System.out.println("Updated Account Balance is" + AccountBalance);
+            return 0;
+
+        } else {
+            System.out.println("Please Enter a Vaild Amount");
+            return 0;
+        }
+    }
+
+    @Override
+    public double withdraw(double amount) {
+        if (AccountBalance > amount) {
+            this.AccountBalance = AccountBalance - amount;
+            System.out.println("Rs." + amount + " WithDrawn");
+            System.out.println("Updated Account Balance is" + AccountBalance);
+            return 0;
+        } else {
+            System.out.println("Not Enough Balance");
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean verifyPin(int atmPin) {
+        if (atmPin == this.Pin) {
+            System.out.println("Verified");
+
+            return true;
+        } else {
+            System.out.println("Wrong PIN");
+
+            return false;
+        }
+    }
+
+    @Override
+    public void showBalance() {
+        System.out.println(AccountBalance);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,47 @@
+import MERAbank.MeraAtm;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            int i = 0;
+            Scanner sc = new Scanner(System.in);
+            MeraAtm obj = new MeraAtm();
+            System.out.println("Please Enter Your PIN");
+            int pin = sc.nextInt();
+            if (obj.verifyPin(pin)) {
+            } else return;
+            do {
+                System.out.println("SHOW Balance -- 1\t Deposit money ----2\t Withdraw Money -----3\t");
+                int temp = sc.nextInt();
+                switch (temp) {
+                    case 1:
+                        obj.showBalance();
+                        break;
+                    case 2:
+                        System.out.println("Enter amount to be deposited");
+                        double amt = sc.nextDouble();
+                        obj.deposit(amt);
+                        break;
+                    case 3:
+                        System.out.println("Enter amount to be Withdrawn");
+                        double amot = sc.nextDouble();
+                        obj.withdraw(amot);
+                        break;
+                    default:
+                        System.out.println("Invaild Option");
+                        break;
 
+
+                }
+                System.out.println("\n\n\nDO you want to continue \n NO ---> 0\t YES ---> any other number");
+                i = sc.nextInt();
+            } while (i != 0);
+        } catch (InputMismatchException e) {
+            System.out.println("Unrecognisable Input Detected Exiting Automatically");
+        }
 
     }
 
