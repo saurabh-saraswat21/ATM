13551bb4f2f632aeaeefbe6d9b0fa40d6269dc66
saurabh <ssroyal>
2020-01-15 18:03:27 +0530

    Add  package IOBbank and a class IOBatm and MERAbank

---
+++ src/IOBbank/YourAccount.java
@@ -0,0 +1,12 @@
+package IOBbank;
+
+public class YourAccount {
+    long Accountno = 00000;
+
+    public void CreateNewAccount(String Name) {
+        Accountno = Accountno + 1;
+        System.out.println("Hey Saurabh Account Sucessfully Created Account no is" + Accountno);
+
+
+    }
+}
