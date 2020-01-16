13bd37758cf4592c4e14787112992b71a31956b8
saurabh <ssroyal>
2020-01-16 10:56:16 +0530

    Add feature to ask PIN 2 times remove asking for PIN after Each Operation

--- src/Main.java
+++ src/Main.java
@@ -6,43 +6,49 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {
-            int i = 0;
+            int i = 3;
             Scanner sc = new Scanner(System.in);
             MeraAtm obj = new MeraAtm();
-            do {
+            for (int c = 0; c < 2; c++) {
+                if (i == 0) return;
+
                 System.out.println("Please Enter Your PIN");
                 int pin = sc.nextInt();
                 if (obj.verifyPin(pin)) {
-                } else return;
-                System.out.println("SHOW Balance -- 1\t Deposit money ----2\t Withdraw Money -----3\t Change PIN----4");
-                int temp = sc.nextInt();
-                switch (temp) {
-                    case 1:
-                        obj.showBalance();
-                        break;
-                    case 2:
-                        System.out.println("Enter amount to be deposited");
-                        double amt = sc.nextDouble();
-                        obj.deposit(amt);
-                        break;
-                    case 3:
-                        System.out.println("Enter amount to be Withdrawn");
-                        double amot = sc.nextDouble();
-                        obj.withdraw(amot);
-                        break;
-                    case 4:
-                        int newpin = sc.nextInt();
-                        obj.ChangePin(pin, newpin);
-                        break;
-                    default:
-                        System.out.println("Invaild Option");
-                        break;
+                } else continue;
+
+                do {
+
+                    System.out.println("SHOW Balance -- 1\t Deposit money ----2\t Withdraw Money -----3\t Change PIN----4");
+                    int temp = sc.nextInt();
+                    switch (temp) {
+                        case 1:
+                            obj.showBalance();
+                            break;
+                        case 2:
+                            System.out.println("Enter amount to be deposited");
+                            double amt = sc.nextDouble();
+                            obj.deposit(amt);
+                            break;
+                        case 3:
+                            System.out.println("Enter amount to be Withdrawn");
+                            double amot = sc.nextDouble();
+                            obj.withdraw(amot);
+                            break;
+                        case 4:
+                            int newpin = sc.nextInt();
+                            obj.ChangePin(pin, newpin);
+                            break;
+                        default:
+                            System.out.println("Invaild Option");
+                            break;
 
 
-                }
-                System.out.println("\n\n\nDO you want to continue \n NO ---> 0\t YES ---> any other number");
-                i = sc.nextInt();
-            } while (i != 0);
+                    }
+                    System.out.println("\n\n\nDO you want to continue \n NO ---> 0\t YES ---> any other number");
+                    i = sc.nextInt();
+                } while (i != 0);
+            }
         } catch (InputMismatchException e) {
             System.out.println("Unrecognisable Input Detected Exiting Automatically");
         }
